ed9c1610b06c9753938f5772a48e3ebba596c566
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2023-12-11 02:09:04 +0530

    few changes in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+
+      <div id='logo'>
+        <h1>Loki's Fleet</h1>
+      </div>
+
+      <div id="textblock">
+        <p>Welcome Earthly species!
+          <br />Looking for a vehicle to travel & explore the Multiverse.
+          <br />Well, Lucky you cause I've got a fleet of my own!
+        </p>
+        <p>
+          There's no term & conditions! <br />I know <span>everything..</span>
+        </p>
+      </div>
+      <div id="scaletext">
+        <p>GOD of MULTIVERSE</p>
+      </div>
+
+    </main>
+  )
+}
+
+export default App
