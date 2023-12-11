5ba11ba2a4345344043efe0af3e96d8f845f1a34
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2023-12-11 13:47:34 +0530

    added parallax scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 function App() {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  })
+
+  const opacity = useTransform(scrollYProgress, [0.2, 0.6], [1, 0])
+  const lokiY = useTransform(scrollYProgress, [0, 0.5, 1], ["0%", "-12%", "-16%"])
 
   return (
     <main>
 
-      <div id='logo'>
-        <h1>Loki's Fleet</h1>
+      {/* parallax section */}
+      <div ref={ref} className="w-full h-[180vh] relative grid place-items-center overflow-y-auto overflow-x-hidden">
+        <motion.div className='block relative z-10 pb-[125vh]' id='logo'
+        style={{
+          opacity: opacity,
+        }}>
+          <h1 className='text-9xl font-extrabold'>Loki's Fleet</h1>
+        </motion.div>
+        <div className='absolute inset-0 z-0'
+          style={{
+            backgroundImage: `url(./src/assets/tree-yggdrassil.png)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+          }} ></div>
+
+        <motion.div className='absolute inset-0 z-20'
+          style={{
+            backgroundImage: `url(./src/assets/intro-loki-bg.png)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: lokiY,
+          }} ></motion.div>
+
       </div>
+
 
       <div id="textblock">
         <p>Welcome Earthly species!
