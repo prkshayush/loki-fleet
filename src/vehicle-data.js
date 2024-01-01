e492f93896c6a5b3ed1c5c5f69cd7c31bd42e177
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2024-01-02 01:06:45 +0530

    slight changes
    changes in image directory and some fixes

--- src/vehicle-data.js
+++ src/vehicle-data.js
@@ -3,24 +3,24 @@ export const vehicles = [
         title: "Audi E-Tron",
         description: "Featured in Avengers: Endgame, driven by Tony Stark, this is an absolute masterpiece and has the best driving expirience. It is spacious, offers supreme luxury, and best range in segment.",
         avaiability: "Available",
-        image: "src/assets/car-1.png"
+        image: "./assets/car-1.png"
     },
     {
         title: "Quinjet",
         description: "This is the jet used by Avengers, belongs to the shield and an extremely powerful machine than has the capability to take you to your destination safe & sound.",
         avaiability: "Will be available soon",
-        image: "/assets/quinjet.png"
+        image: "./assets/quinjet.png"
     },
     {
         title: "Wakandan Jet",
         description: "An absolutely beautiful piece of engineering, used by the people of Wakanda is a joy to ride with. Made with vibranium & power by self renewable source of energy, this is the best choice.",
         avaiability: "Available",
-        image: "./panther-jet.png"
+        image: "./assets/panther-jet.png"
     },
     {
         title: "Jetski",
         description: "Mobius loves Jetskis. Choose this if you love jetskis too. It can not only ride on water surface but can be used as a submarine under water.",
         avaiability: "Not Available",
-        image: "./jetski.png"
+        image: "./assets/jetski.png"
     },
 ]
