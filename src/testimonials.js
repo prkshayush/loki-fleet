31f7d95775a0f41717650877c012e7b8b059f7aa
Ayush Prakash <92213261+prkshayush@users.noreply.github.com>
2024-01-02 01:28:42 +0530

    final changes
    changes in image format & some fixes for faster loading

--- src/testimonials.js
+++ src/testimonials.js
@@ -3,30 +3,30 @@ export const testimonials = [
         name: "O.B",
         place: "Maintenance Division",
         description: "Having years of experience & expertise, I'm incharge for maintenance & safety of the vehicles. I'm available anywhere at anytime in case of emergency.",
-        src: "./assets/ob.png"
+        src: "./assets/ob.webp"
     },
     {
         name: "Thor",
         place: "Customer Support",
         description: "When Thor is here, you don't need to worry about anything. Just book a ride & gear up for an amazing driving experience.",
-        src: "./assets/thor.png"
+        src: "./assets/thor.webp"
     },
     {
         name: "Lady Sif",
         place: "Customer Support",
         description: "Need special customisation or accomodation in your vehicle? Fill the form & I'll be at your service whenever necessary.",
-        src: "./assets/sif.png"
+        src: "./assets/sif.webp"
     },
     {
         name: "Mother",
         place: "Consultant",
         description: "If you aren't able to decide which one to pick for your next ride or have a query, ring at my desk & get all yiur questions answererd.",
-        src: "./assets/mother.png"
+        src: "./assets/mother.webp"
     },
     {
         name: "Mobius",
         place: "Manager",
         description: "Hello! I'm Mobius, Manager of Loki's Fleet. Choose from the variety of vehicles & select a date accordingly. Holiday offers starting soon!",
-        src: "./assets/mobius.png"
+        src: "./assets/mobius.webp"
     },
 ]
